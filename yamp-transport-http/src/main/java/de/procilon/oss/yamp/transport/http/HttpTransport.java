2538f2c0671a854fe395dd482b124b613c10e201
Max Fichtelmann <max.fichtelmann@procilon.de>
2020-04-14 11:11:09 +0200

    response type validation, TLS auth

--- yamp-transport-http/src/main/java/de/procilon/oss/yamp/transport/http/HttpTransport.java
+++ yamp-transport-http/src/main/java/de/procilon/oss/yamp/transport/http/HttpTransport.java
@@ -7,8 +7,16 @@ import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.util.concurrent.CompletionStage;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import de.procilon.oss.yamp.YampException;
 import de.procilon.oss.yamp.YampMediaType;
 import de.procilon.oss.yamp.api.caller.YampTransport;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +25,12 @@ import lombok.RequiredArgsConstructor;
 public class HttpTransport implements YampTransport
 {
     private final URI        remoteEndpoint;
-    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private final HttpClient httpClient;
+    
+    public HttpTransport( URI remoteEndpoint )
+    {
+        this( remoteEndpoint, HttpClient.newHttpClient() );
+    }
     
     @Override
     public CompletionStage<ByteBuffer> transmit( ByteBuffer request )
@@ -31,10 +44,45 @@ public class HttpTransport implements YampTransport
                 .header( "Accept", YampMediaType.RESPONSE_CONTENT )
                 .build();
         
-        // TODO handle non-yamp responses
-        
         return httpClient.sendAsync( httpRequest, BodyHandlers.ofByteArray() )
-                .thenApply( HttpResponse::body )
+                .thenApply( HttpTransport::body )
                 .thenApply( ByteBuffer::wrap );
+    }
+    
+    private static <T> T body( HttpResponse<T> response )
+    {
+        String contentType = response.headers().firstValue( "Content-Type" ).orElse( "" );
+        if ( YampMediaType.RESPONSE_CONTENT.equals( contentType ) )
+        {
+            return response.body();
+        }
+        else
+        {
+            throw new IllegalArgumentException( "expected response of type " + YampMediaType.RESPONSE_CONTENT );
+        }
+    }
+    
+    public static HttpTransport authenticatedTlsTransport( URI remoteEndpoint, KeyStore truststore, KeyStore keystore, char[] keyPassword )
+    {
+        try
+        {
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance( KeyManagerFactory.getDefaultAlgorithm() );
+            kmf.init( keystore, keyPassword );
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance( TrustManagerFactory.getDefaultAlgorithm() );
+            tmf.init( truststore );
+            
+            SSLContext tlsContext = SSLContext.getInstance( "TLSv1.2" );
+            tlsContext.init( kmf.getKeyManagers(), tmf.getTrustManagers(), SecureRandom.getInstanceStrong() );
+            
+            HttpClient httpClient = HttpClient.newBuilder()
+                    .sslContext( tlsContext )
+                    .build();
+            
+            return new HttpTransport( remoteEndpoint, httpClient );
+        }
+        catch ( GeneralSecurityException e )
+        {
+            throw new YampException( "failed to create TLS transport", e );
+        }
     }
 }
