fe9548681ce650411bdc13a46f83413d1bc8fa8b
Max Fichtelmann <max.fichtelmann@procilon.de>
2020-07-14 18:01:00 +0200

    javadoc for top-level classes

--- yamp/src/main/java/de/procilon/oss/yamp/YampMediaType.java
+++ yamp/src/main/java/de/procilon/oss/yamp/YampMediaType.java
@@ -1,10 +1,22 @@
 package de.procilon.oss.yamp;
 
+import de.procilon.oss.yamp.serialization.RequestContainer;
+import de.procilon.oss.yamp.serialization.ResponseContainer;
+
 /**
+ * YAMP specific mime types.
+ * 
  * @author wolffs
  */
 public final class YampMediaType
 {
+    /**
+     * The mime type corresponding to an encoded YAMP {@link RequestContainer}
+     */
     public static final String RESPONSE_CONTENT = "application/yamp-response";
+    
+    /**
+     * The mime type corresponding to an encoded YAMP {@link ResponseContainer}
+     */
     public static final String REQUEST_CONTENT  = "application/yamp-request";
 }
